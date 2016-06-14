bd1b84fb1589756c866a2e046f910925aeedd978
Kitt Hsu <kitt.hsu@gmail.com>
2016-06-14 22:07:40 +0800

    fix and clean timer

--- runtime/timer.js
+++ runtime/timer.js
@@ -1,71 +1,85 @@
 'use strict';
 
-module.exports = Timer;
-
-function Timer() {
-  this.timeouts = new TimerContainer(setTimeout, clearTimeout);
-  this.intervals = new TimerContainer(setInterval, clearInterval);
-}
-
-Timer.prototype.init = function() {
-  this.on('exit', this.clearAllTimers);
-};
-
-Timer.prototype.setTimeout = function(fn, delay) {
-  return this.timeouts.set(fn, delay);
-};
-
-Timer.prototype.clearTimeout = function(id) {
-  this.timeouts.clear(id);
-};
-
-Timer.prototype.setInterval = function(fn, delay) {
-  return this.intervals.set(fn, delay);
-};
-
-Timer.prototype.clearInterval = function(id) {
-  this.intervals.clear(id);
-};
-
-Timer.prototype.clearAllTimers = function() {
-  this.timeouts.clearAll();
-  this.intervals.clearAll();
-};
-
-function TimerContainer(setter, cleaner) {
-  this.setter = setter;
-  this.cleaner = cleaner;
-  this.timers = [];
-  this.idles = [];
-}
-
-TimerContainer.prototype.set = function(fn, delay) {
-  var id;
-  if (this.idles.length > 0) {
-    id = this.idles[this.idles.length - 1];
-    this.idles.pop();
-    if (this.timers[id]) throw new Error('The timer ID is occupied!');
-    this.timers[id] = this.setter(fn, delay);
-  } else {
-    id = this.timers.length;
-    this.timers.push(this.setter(fn, delay));
+class Timer {
+  constructor() {
+    this.timeouts = new TimerContainer(setTimeout, clearTimeout, true);
+    this.intervals = new TimerContainer(setInterval, clearInterval);
   }
 
-  return id;
-};
+  init() {
+    this.on('exit', this.clearAllTimers);
+  }
 
-TimerContainer.prototype.clear = function(id) {
-  if (id === undefined)
-    throw new Error('You should set ID of the timer you want to cancel!');
-  var timerID = this.timers[id];
-  if (timerID === undefined)
-    throw new Error('The timer ID you offered is not correct!');
-  this.cleaner(timerID);
-  this.idles.push(id);
-  this.timers[id] = null;
-};
+  setTimeout(fn, delay) {
+    return this.timeouts.set(fn, delay);
+  }
 
-TimerContainer.prototype.clearAll = function() {
-  this.idles = [];
-  this.timers.forEach((id) => this.cleaner(id));
-};
+  clearTimeout(id) {
+    this.timeouts.clear(id);
+  }
+
+  setInterval(fn, delay) {
+    return this.intervals.set(fn, delay);
+  }
+
+  clearInterval(id) {
+    this.intervals.clear(id);
+  }
+
+  clearAllTimers() {
+    this.timeouts.clearAll();
+    this.intervals.clearAll();
+  }
+}
+
+class TimerContainer {
+  constructor(setter, cleaner, autoCycle) {
+    this.setter = setter;
+    this.cleaner = cleaner;
+    this.timers = [];
+    this.idles = [];
+    this.autoCycle = autoCycle;
+  }
+
+  set(fn, delay) {
+    var id;
+    if (this.idles.length > 0) {
+      id = this.idles[this.idles.length - 1];
+      this.idles.pop();
+      if (this.timers[id]) throw new Error('The timer ID is occupied!');
+      if (this.autoCycle) {
+        delay = () => {
+          delay();
+          this.clear(id);
+        };
+      }
+      this.timers[id] = this.setter(fn, delay);
+    } else {
+      id = this.timers.length;
+      this.timers.push(this.setter(fn, delay));
+    }
+
+    return id;
+  }
+
+  clear(id) {
+    if (id === undefined)
+      throw new Error('You should set ID of the timer you want to cancel!');
+    var timerID = this.timers[id];
+    if (timerID === undefined) {
+      if (this.autoCycle) return;
+      throw new Error('The timer ID you offered is not correct!');
+    }
+
+    this.cleaner(timerID);
+    this.idles.push(id);
+    this.timers[id] = null;
+  }
+
+  clearAll() {
+    this.idles = [];
+    this.timers.forEach((id) => this.cleaner(id));
+  }
+}
+
+module.exports = Timer;
